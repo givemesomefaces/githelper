372a72134e19add5271497e0eb68cb814f615ef7
GitHub Action <action@github.com>
2022-01-03 08:53:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/lvlifeng/githelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lvlifeng/githelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lvlifeng.githelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lvlifeng.githelper.MyBundle
 
 class MyApplicationService {
 
