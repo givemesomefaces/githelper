0dc3d1f7f5639e3f0b58adeca3833394c9a80115
Lv Lifeng <lvlifen9@gmail.com>
2022-01-05 20:30:29 +0800

    commit

---
+++ src/main/kotlin/com/github/lvlifeng/githelper/icons/Icons.kt
@@ -0,0 +1,10 @@
+package com.github.lvlifeng.githelper.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+
+    @JvmField
+    val ToolWindow = IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
+
+}
