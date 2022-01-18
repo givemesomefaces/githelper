8ca8270482a3fc0958187a585fe82d2a735f4a46
Lv Lifeng <lvlifen9@gmail.com>
2022-01-18 21:02:51 +0800

    fix(ui):checkAllBox

--- src/main/java/gitlab/ui/GitLabDialog.java
+++ src/main/java/gitlab/ui/GitLabDialog.java
@@ -206,9 +206,7 @@ public class GitLabDialog extends DialogWrapper {
         if (branchNameRadioButton.isSelected()) {
             initProjectList(filterProjectListByBranch(searchWord));
         }
-        if (selectedProjectList.size() == projectDtoList.size()
-                || selectedProjectList.size() == projectDtoListByBranch.size()
-                || selectedProjectList.size() == filterProjectList.size()) {
+        if (selectedProjectList.containsAll(filterProjectList)) {
             selectAllCheckBox.setSelected(true);
         } else {
             selectAllCheckBox.setSelected(false);
