7e7728ec7c3bd32ec3c4ac15e264d9d2b570d24c
Lv Lifeng <lvlifen9@gmail.com>
2023-02-17 20:14:15 +0800

    feat(server): add select all

--- src/main/java/gitlab/ui/GitLabServersDialog.java
+++ src/main/java/gitlab/ui/GitLabServersDialog.java
@@ -1,31 +1,11 @@
 package gitlab.ui;
 
-import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollectionUtil;
-import com.github.lvlifeng.githelper.Bundle;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vfs.VirtualFile;
-import git4idea.GitUtil;
-import git4idea.repo.GitRepository;
 import gitlab.bean.GitlabServer;
-import gitlab.bean.MergeRequest;
-import gitlab.bean.ProjectDto;
-import gitlab.bean.SelectedProjectDto;
-import gitlab.bean.User;
-import gitlab.helper.RepositoryHelper;
-import gitlab.helper.UsersHelper;
-import org.apache.commons.compress.utils.Lists;
 import org.apache.commons.lang3.StringUtils;
-import org.gitlab.api.models.GitlabBranch;
-import org.gitlab.api.models.GitlabMergeRequest;
-import org.gitlab.api.models.GitlabTag;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import window.LcheckBox;
 
@@ -36,16 +16,13 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import static gitlab.common.Constants.NAME_SPLIT_SYMBOL;
@@ -77,7 +54,7 @@ public class GitLabServersDialog extends DialogWrapper {
     private Set<GitlabServer> selectedGitlabServerList = new HashSet<>();
     private Project project;
 
-    private List<GitlabServer> filterProjectList = new ArrayList<>();
+    private List<GitlabServer> filterGitLabServerList = new ArrayList<>();
 
     public GitLabServersDialog(@Nullable Project project, List<GitlabServer> gitlabServerList) {
         super(project, null, true, DialogWrapper.IdeModalityType.IDE, false);
@@ -87,7 +64,7 @@ public class GitLabServersDialog extends DialogWrapper {
         this.gitlabServerList = gitlabServerList;
         getProjectListAndSortByName();
         initSearch();
-        initServerList(filterProjectsByProject(null));
+        initServerList(filterServersByProject(null));
         initSelectAllCheckBox();
         getRootPane().setDefaultButton(cancelButton);
         initBottomButton();
@@ -112,7 +89,7 @@ public class GitLabServersDialog extends DialogWrapper {
                 return StringUtils.compareIgnoreCase(o1.getRepositoryUrl(), o2.getRepositoryUrl());
             }
         });
-        initServerList(filterProjectsByProject(null));
+        initServerList(filterServersByProject(null));
     }
 
     private void unEnableOtherButtonWhenLoadingData() {
@@ -160,7 +137,46 @@ public class GitLabServersDialog extends DialogWrapper {
         });
     }
 
+    private void searchProject(KeyEvent e) {
+        String searchWord = ((JTextField) e.getSource()).getText();
 
+        initServerList(filterServersByProject(searchWord));
+        if (selectedGitlabServerList.containsAll(filterGitLabServerList) && CollectionUtil.isNotEmpty(filterGitLabServerList)) {
+            selectAllCheckBox.setSelected(true);
+        } else {
+            selectAllCheckBox.setSelected(false);
+        }
+
+    }
+
+    private void initSelectAllCheckBox() {
+        selectAllCheckBox.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (e.getButton() != 1) {
+                    return;
+                }
+                JCheckBox jCheckBox = (JCheckBox) e.getSource();
+                if (jCheckBox.isSelected()) {
+                    selectedGitlabServerList.addAll(filterGitLabServerList);
+                    gitlabServers.addSelectionInterval(0, filterGitLabServerList.size());
+                } else {
+                    selectedGitlabServerList.removeAll(filterGitLabServerList);
+                    gitlabServers.clearSelection();
+                }
+                setSelectedCount();
+                bottomButtonState();
+            }
+        });
+    }
+
+    private void clearSelected() {
+        selectedGitlabServerList.clear();
+        gitlabServers.clearSelection();
+        selectAllCheckBox.setSelected(false);
+        selectedCount.setText("(0 Selected)");
+    }
 
     private void onCancel() {
         // add your code here if necessary
