e331bdfa94fc0e36287879399054ed480a3e946b
Lv Lifeng <lvlifen9@gmail.com>
2023-04-03 19:11:49 +0800

    refactor(reusult): add notifier dialog

---
+++ src/main/java/gitlab/actions/OpenResultDialogAction.java
@@ -0,0 +1,31 @@
+package gitlab.actions;
+
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import gitlab.helper.ActionHelper;
+import gitlab.ui.ResultDialog;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Lv Lifeng
+ * @date 2023-04-02 18:02
+ */
+public class OpenResultDialogAction extends AnAction implements DumbAware {
+
+
+    public OpenResultDialogAction() {
+        super("Details", "Details about Gitlab operation", null);
+    }
+
+    private static void showDetailsFor(Project project) {
+        new ResultDialog(null, null).showAndGet();
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+        ActionHelper.getProject(event).ifPresent(OpenResultDialogAction::showDetailsFor);
+    }
+}
